bb9473955369049a18608ac196227d43fa66b9b7
Ciprian Iloc <ciprian.iloc@softvision.ro>
2017-10-27 17:18:45 +0300

    Setup proj

---
+++ cellpex/cellpex/LoginViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  cellpex
+//
+//  Created by Ciprian Iloc on 27/10/2017.
+//  Copyright © 2017 Ciprian Iloc. All rights reserved.
+//
+
+import UIKit
+import SafariServices
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBAction func signInButtonAction(_ sender: Any) {
+    }
+    
+    @IBAction func forgotButtonAction(_ sender: Any) {
+        let forgotURL = "https://www.cellpex.com/index.php?fuseaction=site.forgot"
+        if let url = URL(string: forgotURL) {
+            let svc = SFSafariViewController(url: url)
+            self.present(svc, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func registerButtonAction(_ sender: Any) {
+        let registerURL = "https://www.cellpex.com/index.php?fuseaction=site.register"
+        if let url = URL(string: registerURL) {
+            let svc = SFSafariViewController(url: url)
+            self.present(svc, animated: true, completion: nil)
+        }
+    }
+}
+
