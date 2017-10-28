5b0f8bf1bc15674de9a9117cb7e74346e10e97bb
Ciprian Iloc <cipriloc@yahoo.com>
2017-10-28 14:13:41 +0300

    Add collection view + testing data

--- cellpex/cellpex/UI/HomeScreen/HomeViewController.swift
+++ cellpex/cellpex/UI/HomeScreen/HomeViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    @IBOutlet weak var colectionView: UICollectionView!
     let searchController = UISearchController(searchResultsController: nil)
 
     override func viewDidLoad() {
@@ -24,7 +25,7 @@ class HomeViewController: UIViewController {
         let leftBarButton = UIBarButtonItem.init(image: UIImage.init(named: "hamburger_icon"), style: .plain, target: self, action: #selector(HomeViewController.back(sender:)))
         self.navigationItem.leftBarButtonItem = leftBarButton
     }
-    
+ 
     @objc func back(sender: UIBarButtonItem) {
 
         _ = navigationController?.popViewController(animated: true)
@@ -34,5 +35,42 @@ class HomeViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+}
+
+extension HomeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 24
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: "ProductCollectionViewCell", for: indexPath) as! ProductCollectionViewCell
+        cell.productImageView.image = UIImage.init(named: "teset_product_icon")
+        cell.productDateLabel.text = "28, Oct"
+        cell.productProvider.text = "servicegsm"
+        cell.productSatusLabel.text = "New"
+        cell.productPriceLabel.text = "320 USD"
+        cell.productDescriptionLabel.text = "Apple iPhone 6s 32G"
+        return cell;
+    }
+    
+}
+
+extension HomeViewController: UICollectionViewDelegate {
 
 }
+
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let widthPerItem = (UIDevice.current.userInterfaceIdiom == .pad) ?(collectionView.frame.width - 20)/2 : collectionView.frame.width - 10;
+        return CGSize(width: widthPerItem, height: 100)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 5;
+    }
+    
+}
+
---
+++ cellpex/cellpex/UI/HomeScreen/ProductCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCollectionViewCell.swift
+//  cellpex
+//
+//  Created by Ciprian Iloc on 10/28/17.
+//  Copyright © 2017 Ciprian Iloc. All rights reserved.
+//
+
+import UIKit
+
+class ProductCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var productImageView: UIImageView!
+    @IBOutlet weak var productDescriptionLabel: UILabel!
+    @IBOutlet weak var productProvider: UILabel!
+    @IBOutlet weak var productPriceLabel: UILabel!
+    @IBOutlet weak var productDateLabel: UILabel!
+    @IBOutlet weak var productSatusLabel: UILabel!
+}
