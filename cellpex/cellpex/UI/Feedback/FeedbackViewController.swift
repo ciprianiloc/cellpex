81b012b3a4884f9c570b026ceab88f9feba70f71
Ciprian Iloc <ciprian.iloc@softvision.ro>
2017-10-31 13:50:39 +0200

    Feedback screen - update

--- cellpex/cellpex/UI/Feedback/FeedbackViewController.swift
+++ cellpex/cellpex/UI/Feedback/FeedbackViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class FeedbackViewController: UIViewController {
-
+    @IBOutlet weak var subjectTextField: UITextField!
+    @IBOutlet weak var messageTextView: UITextView!
+    @IBOutlet weak var sendButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.setNavigationBarHidden(false, animated: false)
@@ -17,9 +19,15 @@ class FeedbackViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func sendButtonAction(_ sender: Any) {
+        
     }
+}
+
+extension FeedbackViewController : UITextViewDelegate {
+    
+}
+
+extension FeedbackViewController : UITextFieldDelegate {
     
 }
