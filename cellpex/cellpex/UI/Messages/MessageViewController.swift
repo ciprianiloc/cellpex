4810d12af2fcdc0f93a37ff5950b01aafe36f205
Ciprian Iloc <cipriloc@yahoo.com>
2017-11-07 22:24:11 +0200

    update message screen

--- cellpex/cellpex/UI/Messages/MessageViewController.swift
+++ cellpex/cellpex/UI/Messages/MessageViewController.swift
@@ -15,16 +15,32 @@ class MessageViewController: UIViewController {
     @IBOutlet weak var messageLabel: UILabel!
     @IBOutlet weak var messageTextView: UITextView!
     @IBOutlet weak var messageTextViewHeight: NSLayoutConstraint!
-    
+    @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var contentView: UIView!
+    let placeholderLabel = UILabel()
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        NotificationCenter.default.addObserver(self, selector: #selector(self.updateTextViewHeight), name: NSNotification.Name.UITextViewTextDidChange, object: nil)
+        sendButton.isEnabled = (messageTextView.text.isEmpty == false)
+        placeholderLabel.text = "Type here..."
+        placeholderLabel.font = UIFont.italicSystemFont(ofSize: (messageTextView.font?.pointSize)!)
+        placeholderLabel.sizeToFit()
+        messageTextView.addSubview(placeholderLabel)
+        placeholderLabel.frame.origin = CGPoint(x: 5, y: (messageTextView.font?.pointSize)! / 2)
+        placeholderLabel.textColor = UIColor.lightGray
+        placeholderLabel.isHidden = !messageTextView.text.isEmpty
+        NotificationCenter.default.addObserver(self, selector: #selector(self.textViewHasChanged), name: NSNotification.Name.UITextViewTextDidChange, object: nil)
     }
 
-    @objc private func updateTextViewHeight() {
+    @objc private func textViewHasChanged() {
+        placeholderLabel.isHidden = !messageTextView.text.isEmpty
         let sizeThatFitsTextView = messageTextView.sizeThatFits(CGSize(width: messageTextView.frame.size.width, height: CGFloat.greatestFiniteMagnitude))
         messageTextViewHeight.constant = sizeThatFitsTextView.height;
+        sendButton.isEnabled = true
+        if messageTextView.text.isEmpty {
+            sendButton.isEnabled = false
+            messageTextViewHeight.constant = 35
+        }
     }
     
     override func didReceiveMemoryWarning() {
@@ -32,5 +48,7 @@ class MessageViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
+    @IBAction func sendMessageAction(_ sender: Any) {
+    }
+    
 }
