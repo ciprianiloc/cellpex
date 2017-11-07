f15e447991dd8b87d03a31ddb8a801c3f1564f74
Ciprian Iloc <ciprian.iloc@softvision.ro>
2017-11-07 16:56:52 +0200

    fix textview color, adjust texiview height according with the length of text

--- cellpex/cellpex/UI/Messages/MessageViewController.swift
+++ cellpex/cellpex/UI/Messages/MessageViewController.swift
@@ -19,23 +19,18 @@ class MessageViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        NotificationCenter.default.addObserver(self, selector: #selector(self.updateTextViewHeight), name: NSNotification.Name.UITextViewTextDidChange, object: nil)
     }
 
+    @objc private func updateTextViewHeight() {
+        let sizeThatFitsTextView = messageTextView.sizeThatFits(CGSize(width: messageTextView.frame.size.width, height: CGFloat.greatestFiniteMagnitude))
+        messageTextViewHeight.constant = sizeThatFitsTextView.height;
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
