014b937942af3eb4af33ec8db742736fa22d08cc
Ciprian Iloc <cipriloc@yahoo.com>
2017-11-08 23:11:01 +0200

    update message screen

--- cellpex/cellpex/UI/Messages/MessageViewController.swift
+++ cellpex/cellpex/UI/Messages/MessageViewController.swift
@@ -8,16 +8,26 @@
 
 import UIKit
 
-class MessageViewController: UIViewController {
+class MessageHeader: UITableViewCell {
     @IBOutlet weak var fromLabel: UILabel!
     @IBOutlet weak var subjectLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
+}
+
+class MessageCell: UITableViewCell {
     @IBOutlet weak var messageLabel: UILabel!
+
+}
+
+
+class MessageViewController: UIViewController {
+
     @IBOutlet weak var messageTextView: UITextView!
     @IBOutlet weak var messageTextViewHeight: NSLayoutConstraint!
+    @IBOutlet weak var messageTextViewButtomConstraint: NSLayoutConstraint!
+    
     @IBOutlet weak var sendButton: UIButton!
-    @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var contentView: UIView!
+    @IBOutlet weak var underLineTextView: UIView!
     let placeholderLabel = UILabel()
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,8 +40,9 @@ class MessageViewController: UIViewController {
         placeholderLabel.textColor = UIColor.lightGray
         placeholderLabel.isHidden = !messageTextView.text.isEmpty
         NotificationCenter.default.addObserver(self, selector: #selector(self.textViewHasChanged), name: NSNotification.Name.UITextViewTextDidChange, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow(sender:)), name:NSNotification.Name.UIKeyboardWillShow, object: nil);
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide(sender:)), name:NSNotification.Name.UIKeyboardWillHide, object: nil);
     }
-
     @objc private func textViewHasChanged() {
         placeholderLabel.isHidden = !messageTextView.text.isEmpty
         let sizeThatFitsTextView = messageTextView.sizeThatFits(CGSize(width: messageTextView.frame.size.width, height: CGFloat.greatestFiniteMagnitude))
@@ -51,4 +62,50 @@ class MessageViewController: UIViewController {
     @IBAction func sendMessageAction(_ sender: Any) {
     }
     
+    @objc func keyboardWillShow(sender: NSNotification) {
+        let userInfo:NSDictionary = sender.userInfo! as NSDictionary
+        let keyboardFrame:NSValue = userInfo.value(forKey: UIKeyboardFrameEndUserInfoKey) as! NSValue
+        let keyboardRectangle = keyboardFrame.cgRectValue
+        let keyboardHeight = keyboardRectangle.height
+        messageTextViewButtomConstraint.constant = keyboardHeight + 10
+    }
+    
+    @objc func keyboardWillHide(sender: NSNotification){
+        messageTextViewButtomConstraint.constant = 10
+    }
+}
+
+extension MessageViewController : UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 120
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MessageHeader") as! MessageHeader
+            cell.fromLabel.text = "TestA@testing.com"
+            cell.dateLabel.text = "18, Oct 2017"
+            cell.subjectLabel.text = "SubjectTest"
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "MessageCell") as! MessageCell
+            cell.messageLabel.text = "Make a symbolic breakpoint at UIViewAlertForUnsatisfiableConstraints to catch this in the debugger.\nMake a symbolic breakpoint at UIViewAlertForUnsatisfiableConstraints to catch this in the debugger."
+
+            return cell
+        }
+    }
+    
+}
+
+extension MessageViewController : UITableViewDelegate {
+
 }
