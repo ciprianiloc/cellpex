f5dd92a00ac08e30b386252538b02221a0180b1a
Ciprian Iloc <ciprian.iloc@softvision.ro>
2017-11-22 17:42:32 +0200

    udpate carusel

--- cellpex/cellpex/UI/ProductDetails/ImagesCarousel.swift
+++ cellpex/cellpex/UI/ProductDetails/ImagesCarousel.swift
@@ -21,13 +21,20 @@ class ImagesCarousel: UIView {
     }
     
     private func loadImageCarousel(){
+        var lastImageViewRight : NSLayoutXAxisAnchor?
         for image in postImages {
             let imageView = UIImageView.init(image: image)
-//            imageView.frame.size = scrollView.frame.size
-//            imageView.frame.origin.y = 0
-//            imageView.autoresizingMask = [.flexibleHeight, .flexibleWidth]
             imageView.contentMode = .scaleAspectFit
             scrollView.addSubview(imageView)
+            imageView.heightAnchor.constraint(equalTo: scrollView.heightAnchor)
+            imageView.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
+            
+            if let previosImageRight = lastImageViewRight {
+                imageView.leftAnchor.constraint(equalTo: previosImageRight)
+            } else {
+                imageView.leftAnchor.constraint(equalTo: scrollView.leftAnchor)
+            }
+            lastImageViewRight = imageView.rightAnchor
         }
     }
 }
