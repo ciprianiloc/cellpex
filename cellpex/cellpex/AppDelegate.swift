734d7dbeca5cd93fafeca0d291a2675e50952b08
Ciprian Iloc <ciprian.iloc@softvision.ro>
2017-11-28 12:28:36 +0200

    integrate firebase analytics

--- cellpex/cellpex/AppDelegate.swift
+++ cellpex/cellpex/AppDelegate.swift
@@ -10,6 +10,7 @@ import UIKit
 import SwiftKeychainWrapper
 import Fabric
 import Crashlytics
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -18,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         Fabric.with([Crashlytics.self])
+        FirebaseApp.configure()
         if (KeychainWrapper.standard.string(forKey: KeychainConstant.deviceID) == nil) {
             KeychainWrapper.standard.set((UIDevice.current.identifierForVendor?.uuidString)!, forKey: KeychainConstant.deviceID)
         }
